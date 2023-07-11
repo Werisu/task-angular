5c692a57dca31c722aeb56f17cca9ba38a034c1d
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-07-11 17:52:32 -0300

    feat: autenticação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
+import { AuthService } from './authentication/auth.service';
 
 @NgModule({
   declarations: [
@@ -18,9 +24,22 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyB7SGEPnROILjRHEc3FXbFT_jndZCHeZ1E",
+      authDomain: "angularmemorando.firebaseapp.com",
+      databaseURL: "https://angularmemorando-default-rtdb.firebaseio.com",
+      projectId: "angularmemorando",
+      storageBucket: "angularmemorando.appspot.com",
+      messagingSenderId: "988276496590",
+      appId: "1:988276496590:web:b11d2ae472455bcf53d89f"
+    })),
+    provideFirestore(() => getFirestore()),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/authentication/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user: any;
+  error: any;
+
+constructor(public auth: AngularFireAuth) { }
+
+async emailSignIn(email: string, password: string) {
+  try {
+    const credential = await this.auth.signInWithEmailAndPassword(email, password);
+    this.user = credential.user;
+  } catch (error) {
+    this.error = error;
+  }
+}
+
+async googleSignIn() {
+  try {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+  } catch (error) {
+    this.error = error;
+  }
+}
+
+}
