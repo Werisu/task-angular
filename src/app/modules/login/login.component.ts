9c2415fcf1fcd61654ec6dc6d1f0e7a70ac90b07
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-07-11 22:43:03 -0300

    feat: adicionando item de lista

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/authentication/auth.service';
-import { Auth, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
+import { Observable, map } from 'rxjs';
+import {
+  AngularFireDatabase,
+  SnapshotAction,
+} from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +15,48 @@ export class LoginComponent implements OnInit {
   email = '';
   password = '';
 
-  constructor(public authx: AuthService, private auth: Auth) {}
+  formNewName = '';
+  formNewAge = '';
+
+  listRef: any;
+  list!: Observable<any[]>;
+
+  constructor(
+    public authx: AuthService,
+    private database: AngularFireDatabase
+  ) {
+    this.listRef = database.list('list');
+    this.list = this.listRef
+      .snapshotChanges()
+      .pipe(
+        map((changes: SnapshotAction<User>[]) =>
+          changes.map((c) => ({key: c.payload.key, ...c.payload.val()})),
+        )
+      );
+  }
 
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
   }
+
+  addItem() {
+    this.listRef.push({
+      name: this.formNewName,
+      age: this.formNewAge,
+      email: this.authx.user.email
+    });
+    this.formNewName = '';
+    this.formNewAge = '';
+  }
+
+  deleteItem(key: string) {
+    this.listRef.remove(key);
+  }
+}
+
+export interface User {
+  key: string;
+  name: string;
+  email: string;
+  age: number;
 }
