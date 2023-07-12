8650141c6689b2f86fec7b4af7067d200619750b
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-07-12 14:07:34 -0300

    feat: adicionando perfil

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@angular/fire/auth';
 import { AuthService } from 'src/app/authentication/auth.service';
 
 @Component({
@@ -6,12 +7,21 @@ import { AuthService } from 'src/app/authentication/auth.service';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  public userLogger!: User;
+  public photoURL!: string;
 
   constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.userLogger = this.authService.userLogged;
+    this.photoURL = this.userLogger.providerData[0].photoURL!;
+  }
 
   signOut() {
     this.authService.signOut();
   }
+
+
 
 }
