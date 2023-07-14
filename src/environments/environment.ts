99e756884013b7035bcfb43d899768f3b117b0d9
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-07-14 07:24:53 -0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyB7SGEPnROILjRHEc3FXbFT_jndZCHeZ1E",
+    authDomain: "angularmemorando.firebaseapp.com",
+    databaseURL: "https://angularmemorando-default-rtdb.firebaseio.com",
+    projectId: "angularmemorando",
+    storageBucket: "angularmemorando.appspot.com",
+    messagingSenderId: "988276496590",
+    appId: "1:988276496590:web:b11d2ae472455bcf53d89f"
+  },
+  production: true
+};
