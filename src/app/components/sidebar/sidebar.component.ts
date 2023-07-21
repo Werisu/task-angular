5e22ce289e2a6ddf1fd3e509f6deb247dc87b783
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-07-21 09:34:18 -0300

    feat: mobile action nav

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth, User } from '@angular/fire/auth';
 import { AuthService } from 'src/app/authentication/auth.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,11 +12,23 @@ export class SidebarComponent implements OnInit {
   public userLogger?: User;
   public photoURL!: string;
   public isAtivo: boolean = true; // Defina o valor da variável com base na lógica do seu aplicativo
+  public showNav: boolean = false;
+  public isMobile: boolean = false;
 
-  constructor(private authService: AuthService, private auth: Auth) { }
+  constructor(private authService: AuthService, private auth: Auth, private bpo: BreakpointObserver) { }
 
   ngOnInit(): void {
     this.getUser();
+  }
+
+  getBreakpoint() {
+    this.bpo.observe(['(max-width: 639px)']).subscribe((res) => {
+      if(res.matches){
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
   }
 
   signOut() {
